6ccff66a2f46b13f3715c671ba84cb16a71f2f40
Andrew <andrewjacobalvarez22@gmail.com>
2023-05-15 15:06:36 -0500

    delete features

--- SimpleTaskTracker/src/app/components/task-item/task-item.component.ts
+++ SimpleTaskTracker/src/app/components/task-item/task-item.component.ts
@@ -10,4 +10,8 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent {
   faTimes = faTimes;
   @Input() 'task': Task;
+
+  deleteClick() {
+    console.log('deleteClick');
+  }
 }
