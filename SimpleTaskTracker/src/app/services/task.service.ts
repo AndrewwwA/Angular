618c52a8fb86c7b5814ffa28ad2e675e5472ce9a
Andrew <andrewjacobalvarez22@gmail.com>
2023-05-16 18:09:38 -0500

    new reminder to change reminder status

--- SimpleTaskTracker/src/app/services/task.service.ts
+++ SimpleTaskTracker/src/app/services/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../TaskInterface';
 
@@ -13,5 +13,13 @@ export class TaskService {
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
+  }
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+  updateReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task);
   }
 }
