2ecc2e59e710eb1b1f505af912f9822b3da8eca0
Andrew <andrewjacobalvarez22@gmail.com>
2023-05-17 17:37:18 -0500

    on submit forms + reset

--- SimpleTaskTracker/src/app/components/add-task/add-task.component.ts
+++ SimpleTaskTracker/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/TaskInterface';
 
 @Component({
   selector: 'app-add-task',
@@ -9,4 +10,24 @@ export class AddTaskComponent {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  @Output() newAddTask: EventEmitter<Task> = new EventEmitter();
+
+  onSubmit(): void {
+    if (!this.text) {
+      alert('Please Add a task');
+      return;
+    } else {
+      const newTask: Task = {
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder,
+      };
+
+      this.newAddTask.emit(newTask);
+
+      this.text = '';
+      this.day = '';
+      this.reminder = false;
+    }
+  }
 }
